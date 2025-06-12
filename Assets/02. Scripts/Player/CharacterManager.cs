153f04bf8f1b7fc7938c1fab477de1c86fe7bcbb
psj45633 <psj45633@gmail.com>
2025-06-12 20:16:02 +0900

    feat : 플레이어 이동 및 점프

---
+++ Assets/02. Scripts/Player/CharacterManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : Singleton<CharacterManager>
+{
+
+    private Player _player;
+    public Player Player
+    {
+        get { return _player; }
+        set { _player = value; }
+    }
+}
---
+++ Assets/02. Scripts/Player/Inputs.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Inputs : MonoBehaviour
+{
+    public PlayerInput playerInput {  get; private set; }
+    public PlayerInput.PlayerActions playerActions { get; private set; }
+
+    public PlayerController controller { get; private set; }
+    private void Awake()
+    {
+        playerInput = new PlayerInput();
+        playerActions = playerInput.Player;
+        controller = GetComponent<PlayerController>();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+        playerInput.Player.Move.performed += controller.OnMoveInputPerformed;
+        playerInput.Player.Move.canceled += controller.OnMoveInputCanceled;
+        playerInput.Player.Look.performed += controller.OnLookInputPerformed;
+        playerInput.Player.Look.canceled += controller.OnLookInputCanceled;
+        playerInput.Player.Jump.started += controller.OnJumpInput;
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+        playerInput.Player.Move.performed -= controller.OnMoveInputPerformed;
+        playerInput.Player.Move.canceled -= controller.OnMoveInputCanceled;
+        playerInput.Player.Look.performed -= controller.OnLookInputPerformed;
+        playerInput.Player.Look.canceled -= controller.OnLookInputCanceled;
+        playerInput.Player.Jump.started -= controller.OnJumpInput;
+
+    }
+}
