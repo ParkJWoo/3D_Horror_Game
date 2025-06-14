be174245719b7e33531068ac32b3453c78f50b7d
chae_kkung <ycy0109@naver.com>
2025-06-14 17:18:24 +0900

    refactor : 불필요한 스크립트 제거

--- Assets/04. ScriptableObjects/Sound/SoundListSO.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Sound/SoundListData")]
-public class SoundListSO : ScriptableObject
-{
-    public string soundListName;
-    public SoundSO[] soundItems;
-}
