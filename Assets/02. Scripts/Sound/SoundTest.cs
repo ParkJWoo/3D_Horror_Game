d6fdbc04264d78d75ca3c5aa6e1d6f7d353fc28a
chae_kkung <ycy0109@naver.com>
2025-06-16 18:14:19 +0900

    fix : 시점 제어로 생긴 오류 수정

--- Assets/02. Scripts/Sound/SoundTest.cs
+++ Assets/02. Scripts/Sound/SoundTest.cs
@@ -9,7 +9,7 @@ public class SoundTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SoundManager.Instance.PlayBgmLoop("DefaultBGM");
+        //SoundManager.Instance.PlayBgmLoop("DefaultBGM");
     }
     
     void Update()
