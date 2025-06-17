640f49e8c5c5b3e0a4e43b075f7324376feaa6cb
ParkJWoo <character453@naver.com>
2025-06-17 16:24:19 +0900

    feat : 임시 가이드 문구 출력 기능 추가 + 사진들 추가 배치 완료

---
+++ Assets/02. Scripts/UIs/ManualGuide.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class ManualGuide : MonoBehaviour
+{
+    public TextMeshProUGUI controlTextUI;
+    public float displayDuration = 4f;
+
+    [TextArea(2, 5)]
+    public List<string> controlMessage = new List<string>();
+
+    private void Start()
+    {
+        if (controlTextUI != null && controlMessage.Count > 0)
+        {
+            StartCoroutine(GuideStart());
+        }
+    }
+
+    private IEnumerator GuideStart()
+    {
+        yield return new WaitForSeconds(2.5f);
+
+        if (controlTextUI != null && controlMessage.Count > 0)
+        {
+            StartCoroutine(ShowControlsRoutine());
+        }
+    }
+
+    private IEnumerator ShowControlsRoutine()
+    {
+        controlTextUI.gameObject.SetActive(true);
+
+        foreach (string msg in controlMessage)
+        {
+            controlTextUI.text = msg;
+            yield return new WaitForSeconds(displayDuration);
+        }
+
+        controlTextUI.gameObject.SetActive(true);
+
+        //  다음 가이드 진행
+        GuideManager.Instance?.StartGuideSequence();
+    }
+}
