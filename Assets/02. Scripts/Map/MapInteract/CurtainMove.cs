51eeb00c7b293c0616b02d50ae36970719d71b91
ChoiGunWoo1 <gunwoo0813@naver.com>
2025-06-17 10:22:02 +0900

    feat : 커튼 올리고 내리기 기능 추가

--- Assets/02. Scripts/Map/MapInteract/CurtainMove.cs
+++ Assets/02. Scripts/Map/MapInteract/CurtainMove.cs
@@ -1,28 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class CurtainMove : MonoBehaviour
+public class CurtainMove : MonoBehaviour, IInteractable
 {
     private float targetUpY = 3f;
     private float targetDownY = 0.5f;
     private bool isCurtainUp = false;
 
-    void Start()
-    {
-        StartCoroutine(CurtainRollUp());
-    }
+    public event Action<IInteractable> OnInteracted;
+
 
     public void CurtainInteract()
     {
         if(isCurtainUp)
         {
             StartCoroutine(CurtainRollDown());
+            isCurtainUp = false;
+            return;
         }
         else
         {
             StartCoroutine(CurtainRollUp());
+            isCurtainUp = true;
+            return;
         }
     }
 
@@ -34,17 +37,27 @@ public class CurtainMove : MonoBehaviour
             transform.position = new Vector3(transform.position.x, newY, transform.position.z);
             yield return null;
         }
+        yield break;
     }
 
     IEnumerator CurtainRollDown()
     {
-        while (transform.position.y > targetUpY + 0.01f)
+        while (transform.position.y > targetDownY + 0.01f)
         {
             float newY = Mathf.MoveTowards(transform.position.y, targetDownY, 3f * Time.deltaTime);
             transform.position = new Vector3(transform.position.x, newY, transform.position.z);
             yield return null;
         }
+        yield break;
     }
 
+    public void SetInterface(bool active)
+    {
+ 
+    }
 
+    public void OnInteraction()
+    {
+        CurtainInteract();
+    }
 }
