a8dcb40fb76247d5c7ab4caa6d0b4e624f4c25b0
ParkJWoo <character453@naver.com>
2025-06-18 01:45:08 +0900

    feat : StartScene 생성

---
+++ Assets/02. Scripts/Common/StartSceneButtonEvents.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartSceneButtonEvents : MonoBehaviour
+{
+    public SceneLoader sceneLoader;
+    public string mainSceneName = "MainScene";
+
+    //  시작 버튼에 연결
+    public void OnStartButtonClicked()
+    {
+        if (sceneLoader != null)
+        {
+            sceneLoader.MoveScene(mainSceneName);
+        }
+
+        else
+        {
+            Debug.LogWarning("[StartSceneButtonEvents] SceneLoader가 할당되지 않음!");
+        }
+    }
+
+    //  게임 종료 버튼에 연결
+    public void OnExitButtonClicked()
+    {
+        Application.Quit();
+    }
+}
